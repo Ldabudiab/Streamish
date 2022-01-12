b288b4c94c438a24330bc24606f2b907d21550de
ldabudiab <ldabudiab@gmail.com>
2022-01-12 09:15:04 -0600

    first commit

---
+++ Streamish/Repositories/IVideoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Streamish.Models;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+        void Add(Video video);
+        void Delete(int id);
+        List<Video> GetAll();
+        List<Video> GetAllWithComments();
+        Video GetById(int id);
+        Video GetVideoByIdWithComments(int id);
+        List<Video> Search(string criterion, bool sortDescending);
+        List<Video> SearchByDate(string searchDate);
+        void Update(Video video);
+    }
+}
